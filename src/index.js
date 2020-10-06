db44f8a3a6741af3fb0113cc13bff1e026459486
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-06 19:59:53 +0300

    refactor: refactor solution to towel sort task

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ module.exports = function towelSort (matrix) {
         for (var i=1; i <matrix.length; i++){
             if ( i%2 !== 0 ){
                 matrix[i].reverse();
-            };
-        };
-        return [].concat(...matrix);
+            }
+        }
+        return [].concat(...matrix)
     }
 
 }
